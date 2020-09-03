4aa469c8a44bb465e1e96875526fb6782afa25a4
Aldin Mujkic <aldinmujkic44@gmail.com>
2020-09-03 11:35:07 +0200

    404 content

---
+++ 404.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ */
+
+get_header();
+?>
+
+
+  <?php
+  /*
+  * default search form / uncoment if you need
+  * get_search_form();
+  *
+  */
+  ?>
+
+
+<!-- include footer -->
+<?php get_footer(); ?>
