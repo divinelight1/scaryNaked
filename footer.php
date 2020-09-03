c1fc982af38182dc10c0e886d4e20ad42a2e550d
Aldin Mujkic <aldinmujkic44@gmail.com>
2020-09-03 11:28:57 +0200

    add some stylesheets,add footer content

---
+++ footer.php
@@ -0,0 +1,12 @@
+    <?php wp_footer(); ?>
+    <!-- bootstrap js cdn -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script>
+    
+    <!-- animations CDN script -->
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+      // initialize animations script
+      AOS.init();
+    </script>
+  </body>
+</html>
--- header.php
+++ header.php
@@ -11,27 +11,30 @@
 <!doctype html>
 <html class="no-js" lang="">
 
-<head>
-  <meta charset="<?php bloginfo( 'charset' ); ?>">
-  <title></title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- this file is used just if you have some custom css / normally all stile files are enqueued in functions.php -->
-  <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/custom.css">
+    <!-- this file is used just if you have some custom css / normally all style files are enqueued in functions.php -->
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/custom.css">
 
-  <!-- mobile browser bar color / match this color with your website highlighted color / or any other that you like -->
-  <meta name="theme-color" content="#fafafa">
+    <!-- mobile browser bar color / match this color with your website highlighted color / or any other that you like -->
+    <meta name="theme-color" content="#fafafa">
 
-  <!-- bootstrap CDN -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <!-- bootstrap CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
-  <!-- font awesome CDN / uncomment if you need -->
-  <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"> -->
+    <!-- font awesome CDN / uncomment if you need -->
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"> -->
 
-  <?php wp_head(); ?>
+    <!-- animations CDN -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
-</head>
+    <?php wp_head(); ?>
 
-<body>
+  </head>
 
-<!-- HERE GOES CONTENT -->
+  <body>
+
+  <!-- HERE GOES CONTENT -->
