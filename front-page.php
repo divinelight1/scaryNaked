37258f8d106adc93f257ac3c54cc029cd6cd0e1b
Aldin Mujkic <aldinmujkic44@gmail.com>
2020-09-03 19:54:44 +0200

    add front-page

---
+++ front-page.php
@@ -0,0 +1,29 @@
+<?php
+/**
+* The template for displaying static homepage
+ *
+ * This is the template that displays static homepage.
+ *
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ */
+
+ get_header();
+ ?>
+
+ <!--
+abowe is the loop for backend editor content
+if you dont need it feel free to delete
+otherwise fit into the markup that you like
+
+<?php if (have_posts()) : ?>
+  <?php while (have_posts()) : the_post(); ?>
+  here you can place markup
+    <?php the_content(); ?>
+  <?php endwhile; ?>
+<?php endif; ?>
+
+ -->
+
+ <?php get_footer(); ?>
