7ec251542431d62c89b5802c3c55e248819a097a
Aldin Mujkic <aldinmujkic44@gmail.com>
2020-09-03 10:55:06 +0200

    content on index and default style files

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+* Root template
+*
+* This is one of two required files for wp theme
+* It's used when nothing other more specific matches a query.
+* For example if you don't have frontpage.php or home.php
+* Personally i always keep it clean (only header and footer include)
+*/
+
+ ?>
+
+ <!-- include header -->
+ <?php include('header.php'); ?>
+
+ <!-- here it goes content, if neccessary -->
+
+<!-- include footer -->
+ <?php include('footer.php'); ?>
